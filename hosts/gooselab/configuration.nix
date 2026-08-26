f79907e545ee691fea82dc68832c9ec752bb4a37
Johnny Urosevic <themagicalcake@gmail.com>
2026-08-26 00:27:49 -0700

    Move hardware configurations into the repo

--- hosts/gooselab/configuration.nix
+++ hosts/gooselab/configuration.nix
@@ -2,8 +2,7 @@
 
 {
   imports = [
-    # Include the results of the hardware scan.
-    /etc/nixos/hardware-configuration.nix
+    ./hardware-configuration.nix
     ../../features
     ../../features/homelab
   ];
@@ -77,7 +76,7 @@
 
   #TODO: move this to home.nix after I start using nix to configure zshrc
   programs.zsh.shellAliases = {
-    build = "sudo nixos-rebuild switch --impure --flake=path:/home/goose/nix/";
+    build = "sudo nixos-rebuild switch --flake=path:/home/goose/nix/";
   };
 
   # This value determines the NixOS release from which the default
