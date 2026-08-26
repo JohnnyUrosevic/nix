e603e1736f7955020941060d760aff246a86d8ea
Johnny Urosevic <themagicalcake@gmail.com>
2026-08-25 22:12:56 -0700

    Get minecraft bedrock working

--- features/homelab/minecraft/minecraft.nix
+++ features/homelab/minecraft/minecraft.nix
@@ -21,7 +21,7 @@
           gamemode = "survival";
           difficulty = "hard";
           motd = "Gonse's Minecraft server!";
-          white-list = true;
+          white-list = false;
           max-players = 50;
         };
 
@@ -29,13 +29,19 @@
           themagicalcake = "9e7dff38-a6fc-47bc-91c9-9fa4e4dc460d";
         };
 
-        symlinks = {
-          "plugins" = pkgs.linkFarm "plugins" {
-            "PasswordWhitelist.jar" = pkgs.fetchurl {
-              url = "https://hangarcdn.papermc.io/plugins/Strokkur424/PasswordWhitelist/versions/v1.0.0/PAPER/PasswordWhitelist-1.0.0.jar";
-              hash = "sha256-2/eK9AzUKWMigWrhBZ+KYBP/tLUlROCTt7Q84kpQm5E=";
-            };
-            "PasswordWhitelist" = ./plugins/PasswordWhitelist;
+        files = {
+          "plugins/PasswordWhitelist.jar" = pkgs.fetchurl {
+            url = "https://hangarcdn.papermc.io/plugins/Strokkur424/PasswordWhitelist/versions/v1.0.0/PAPER/PasswordWhitelist-1.0.0.jar";
+            hash = "sha256-2/eK9AzUKWMigWrhBZ+KYBP/tLUlROCTt7Q84kpQm5E=";
+          };
+          "plugins/PasswordWhitelist/data" = ./plugins/PasswordWhitelist/data;
+          "plugins/Geyser-Spigot.jar" = pkgs.fetchurl {
+            url = "https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot";
+            hash = "sha256-t1S7JXl2VJVTI5voLEsajKl7rJl8h5n97loooofn/aY=";
+          };
+          "plugins/floodgate-spigot.jar" = pkgs.fetchurl {
+            url = "https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/spigot";
+            hash = "sha256-n0NsQv/YsQkaQ316Thb4IYG51TFPixcy36nVpP/7Gf4=";
           };
         };
       };
@@ -44,6 +50,10 @@
 
   networking.firewall.allowedTCPPorts = [
     25565
+    19132
+  ];
+
+  networking.firewall.allowedUDPPorts = [
     19132
   ];
 }
