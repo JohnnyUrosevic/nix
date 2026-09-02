162a82f4650655e62b6491fed5adfd70ff5aeca9
Johnny Urosevic <themagicalcake@gmail.com>
2026-09-01 23:27:04 -0700

    Let nextcloud paths be added to jellyfin

--- hosts/gooselab/configuration.nix
+++ hosts/gooselab/configuration.nix
@@ -41,6 +41,9 @@
     extraGroups = [
       "networkmanager"
       "wheel"
+      "nextcloud"
+      "video"
+      "render"
     ];
     shell = pkgs.zsh;
   };
@@ -80,18 +83,11 @@
 
   networking.enableIPv6 = false;
 
-
   #TODO: move this to home.nix after I start using nix to configure zshrc
   programs.zsh.shellAliases = {
     build = "sudo nixos-rebuild switch --flake=path:/home/goose/nix/ --impure";
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
 }
