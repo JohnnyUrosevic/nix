11352cb08e2899b2015f585575d0d8f2f624c0e6
Johnny Urosevic <themagicalcake@gmail.com>
2026-09-01 23:27:18 -0700

    Hardware acceleration for Jelly

--- features/homelab/arr.nix
+++ features/homelab/arr.nix
@@ -25,5 +25,19 @@ in
     pkgs.jellyfin-ffmpeg
   ];
 
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+
+    extraPackages = with pkgs; [
+      intel-compute-runtime
+      intel-media-driver
+      intel-vaapi-driver
+    ];
+  };
+
+  systemd.services.jellyfin.environment.LIBVA_DRIVER_NAME = "iHD";
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
+
   services.seerr = sharedAttrs;
 }
