de335ac05144477268fb348434a270b630751cf9
Johnny Urosevic <themagicalcake@gmail.com>
2026-09-04 21:36:07 -0700

    Auto upgrade homebrew and its apps

--- hosts/macbook/homebrew.nix
+++ hosts/macbook/homebrew.nix
@@ -1,23 +1,30 @@
 {
-  homebrew.enable = true;
-  homebrew.casks = [
-    "ghostty"
-    "tailscale-app"
-    "discord"
-    "steam"
-    "zen"
-    "google-chrome"
-    "rustdesk"
-    "raycast"
-    "spotify"
-    "obsidian"
-    "godot"
-    "iina"
+  homebrew = {
+    enable = true;
+    casks = [
+      "ghostty"
+      "tailscale-app"
+      "discord"
+      "steam"
+      "zen"
+      "google-chrome"
+      "rustdesk"
+      "raycast"
+      "spotify"
+      "obsidian"
+      "godot"
+      "iina"
 
-    {
-      name = "private-internet-access";
-      trusted = true;
-    }
-  ];
+      {
+        name = "private-internet-access";
+        trusted = true;
+      }
+    ];
 
+    onActivation = {
+      autoUpdate = true;
+      upgrade    = true;
+      cleanup    = "uninstall";
+    };
+  };
 }
